7b9c525fe163bfd03783cd62fd819ccdc05ee06e
tdat <datnguyen4804@gmail.com>
2025-02-22 01:44:47 +0700

    fix-cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const { sequelize, connectDB } = require('./config/database');
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Import models
 require('./models/user');
