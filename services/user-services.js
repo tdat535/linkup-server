5f892f4aecc8d1e79e75edc26734112ecc3e1751
tdat <datnguyen4804@gmail.com>
2025-03-13 20:10:49 +0700

    new

--- services/user-services.js
+++ services/user-services.js
@@ -189,13 +189,13 @@ const createNewAccessToken = async (token) => {
 
 // Hàm tạo Access Token
 const generateAccessToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "15m" });
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "24h" });
 };
 
 // Hàm tạo Refresh Token
 const generateRefreshToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: "1h",
+    expiresIn: "7d",
   });
 };
 
