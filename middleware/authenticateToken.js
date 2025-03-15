88cab54cc5fb6aaeb6fa461f6dd44532f71034ca
tdat <datnguyen4804@gmail.com>
2025-03-15 14:36:50 +0700

    new

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -11,7 +11,7 @@ const authenticateToken = (req, res, next) => {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
             if (err.name === 'TokenExpiredError') {
-                return res.status(401).json({ error: 'Token expired' }); // 🔥 Trả về lỗi rõ ràng
+                return res.status(401).json({ error: 'Token expired' });
             }
             return res.status(403).json({ error: 'Invalid token' });
         }
