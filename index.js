7fea99db473bff9f8324766fab62675da0d062e9
tdat <datnguyen4804@gmail.com>
2025-04-08 10:50:59 +0700

    new

--- index.js
+++ index.js
@@ -17,7 +17,22 @@ const server = http.createServer(app); // Tạo server HTTP
 //   allowedHeaders: ['Content-Type', 'Authorization']
 // };
 
-app.use(cors());
+const allowedOrigins = [
+  "https://linkup.id.vn",
+  "https://admin.linkup.id.vn",
+  "http://localhost:3000",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 const cookieParser = require("cookie-parser");
